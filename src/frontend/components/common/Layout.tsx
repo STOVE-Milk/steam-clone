b14b10f5b09749d9596d207255bcb9cd2437d9a9
minjyo <minjyo8823@gmail.com>
2021-12-29 17:21:46 +0900

    refactor: Header.tsx, Layout.tsx 를 components/common 폴더로 이동

--- src/frontend/components/common/Layout.tsx
+++ src/frontend/components/common/Layout.tsx
@@ -1,4 +1,4 @@
-import Header from 'components/Header';
+import Header from 'components/common/Header';
 import React from 'react';
 
 interface LayoutProps {
--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'styles/global-styles';
 import { theme } from 'styles/theme';
-import Layout from 'components/Layout';
+import Layout from 'components/common/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
