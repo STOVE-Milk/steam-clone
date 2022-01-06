bbb8a3890730c20c649926b090e9cc891686f9b3
minjyo <minjyo8823@gmail.com>
2022-01-06 20:24:24 +0900

    chore: Layout 구조 변경

--- src/frontend/templates/Layout.tsx
+++ src/frontend/templates/Layout.tsx
@@ -20,7 +20,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <MainWrapper>
       <NavBar />
-      {children}
+      <ContentSection>{children}</ContentSection>
     </MainWrapper>
   );
 }
