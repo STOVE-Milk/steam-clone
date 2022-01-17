6908d8516f626cfc112c8e6fe0438dc05aea5780
minjyo <minjyo8823@gmail.com>
2022-01-17 17:43:10 +0900

    feat: action 생성

--- src/frontend/modules/game.ts
+++ src/frontend/modules/game.ts
@@ -1,42 +1,15 @@
-import { createAction, handleActions } from 'redux-actions';
-import { takeLatest } from 'redux-saga/effects';
-import createRequestSaga, {
-  createRequestActionTypes,
-} from 'lib/createRequestSaga';
-import * as postsAPI from 'lib/api/posts';
+import { createAction } from 'typesafe-actions';
 
-const [READ_POST, READ_POST_SUCCESS, READ_POST_FAILURE] =
-  createRequestActionTypes('post/READ_POST');
+import { createRequestActionTypes } from './createRequestSaga';
 
-const UNLOAD_POST = 'post/UNLOAD_POST'; // 포스트 페이지에서 벗어날 때 데이터 비우기
+export const [GET_CATEGORIES, GET_CATEGORIES_SUCCESS, GET_CATEGORIES_FAIL] = 'GET_CATEGORIES' as const;
 
-export const readPost = createAction(READ_POST, (id) => id);
-export const unloadPost = createAction(UNLOAD_POST);
+export const getCategories = createAction(GET_CATEGORIES)();
+// action의 parameter들을 <>에 넣어줌
+// export const setStringArr = createAction(SET_STRING_ARR)<string[]>();
+// export const setObject = createAction(SET_OBJECT)<{
+//   a: string[];
+//   b: number;
+//   c: string;
+// }>();
 
-const readPostSaga = createRequestSaga(READ_POST, postsAPI.readPost);
-export function* postSaga() {
-  yield takeLatest(READ_POST, readPostSaga);
-}
-
-const initialState = {
-  post: null,
-  error: null,
-};
-
-const post = handleActions(
-  {
-    [READ_POST_SUCCESS]: (state, { payload: post }) => ({
-      ...state,
-      post,
-    }),
-    [READ_POST_FAILURE]: (state, { payload: error }) => ({
-      ...state,
-      error,
-    }),
-
-    [UNLOAD_POST]: () => initialState,
-  },
-  initialState,
-);
-
-export default post;
