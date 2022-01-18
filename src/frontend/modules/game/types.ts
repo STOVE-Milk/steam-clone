24121636e065277c0666301c688206db5b78f9e5
minjyo <minjyo8823@gmail.com>
2022-01-18 16:57:31 +0900

    feat: GameInfo 타입 및 initial GameState 추가

--- src/frontend/modules/game/types.ts
+++ src/frontend/modules/game/types.ts
@@ -2,12 +2,25 @@ import { ActionType } from 'typesafe-actions';
 
 import * as actions from './actions';
 
-export type gameState = {
-  // [index: string]: string | string[] | null;
+export type gameInfo = {
+  id: number;
+  name: string;
+  price: {
+    [index: string]: number;
+  };
+  description: string;
+  os: string[];
+  category_list: string[];
+  image?: {};
+  video?: {};
+  sale: number;
+};
 
+export type gameState = {
   categories: string[];
-  // categories: IGetCategoriesResType | null
   categoryError: string;
+
+  games: gameInfo[];
 };
 
 export type gameAction = ActionType<typeof actions>;
