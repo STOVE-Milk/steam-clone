5a5381ce62f9571f438a5db09d89a874e11bd065
abc7468 <abc7468@naver.com>
2022-01-20 00:26:05 +0900

    feat: getReviewList API 추가 및 list 처리

--- src/backend/gRPC-gateway/proto/store.pb.gw.go
+++ src/backend/gRPC-gateway/proto/store.pb.gw.go
@@ -486,9 +486,9 @@ var (
 
 	pattern_Store_GetGame_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"store", "games", "game_id"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Store_GetDiscountingGameList_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"store", "discounting"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Store_GetDiscountingGameList_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"store", "games", "discounting"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Store_GetReviewList_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"store", "game_id", "reviews"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Store_GetReviewList_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"store", "games", "game_id", "reviews"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
