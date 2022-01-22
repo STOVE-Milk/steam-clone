547a882ef69cc11b47fb712fa793c48697189d84
Tae-Hyeon <hanth7028@gmail.com>
2022-01-22 15:41:36 +0900

    chore: 성공 코드 수정 7000 to 77000

--- src/backend/payment/src/main/java/com/steam/payment/global/common/Body.java
+++ src/backend/payment/src/main/java/com/steam/payment/global/common/Body.java
@@ -14,7 +14,7 @@ public class Body<T> {
 
     public static <T> Body<Object> success(T data) {
         return Body.builder()
-                .code(7000)
+                .code(77000)
                 .message("성공")
                 .data(data)
                 .build();
