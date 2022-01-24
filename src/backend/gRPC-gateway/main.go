5a990ed377fc23a4982e298a9c6a83956b7e8b06
abc7468 <abc7468@naver.com>
2022-01-24 20:19:52 +0900

    feat: cors 해결

--- src/backend/gRPC-gateway/main.go
+++ src/backend/gRPC-gateway/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
-	"log"
-	"net/http"
-
 	storePb "github.com/STOVE-Milk/steam-clone/gRPC-gateway/proto"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
+	"log"
+	"net/http"
+	"strings"
 )
 
 const (
@@ -43,6 +43,27 @@ func registerServiceHandlers(ctx context.Context, mux *runtime.ServeMux) error {
 	return nil
 }
 
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	return
+}
+
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
@@ -54,7 +75,7 @@ func main() {
 	registerServiceHandlers(ctx, mux)
 
 	log.Printf("start HTTP server on %s port", gRPCGatewayPortNumber)
-	if err := http.ListenAndServe(":"+gRPCGatewayPortNumber, mux); err != nil {
+	if err := http.ListenAndServe(":"+gRPCGatewayPortNumber, allowCORS(mux)); err != nil {
 		log.Fatalf("failed to serve: %s", err)
 	}
 }
