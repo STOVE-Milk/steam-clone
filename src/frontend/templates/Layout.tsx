0db7cf71ff972bb627fdf72099ef1bbafe2e7759
minjyo <minjyo8823@gmail.com>
2022-01-24 15:21:47 +0900

    feat: stash

--- src/frontend/templates/Layout.tsx
+++ src/frontend/templates/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import NavBar from 'components/organisms/Navbar';
+import NavBar from 'components/organisms/NavBar';
 import Header from 'components/organisms/Header';
 
 interface LayoutProps {
