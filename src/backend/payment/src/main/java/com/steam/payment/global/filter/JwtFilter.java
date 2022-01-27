9e8ef9a8db46b03a6ac74e4798583998a491b42c
Tae-Hyeon <hanth7028@gmail.com>
2022-01-27 16:52:28 +0900

    feat: CORS 에러 수정
    1. 요청 캐시 삭제하여 테스트
       - 안 되어야 할 요청도 성공하여 테스트에 혼란을 줌
    2. 다른 브라우저에서 새로 요청하여 헤더를 출력해보니 요청이 OPTIONS 요청의 헤더들이었다. 
      - 먼저 CORS 처리를 위한 요청이 JwtCheck보다 뒤에 있는 것 같다.
      - 따라서 JwtFilter가 먼저 수행되는 문제가 발생
      - OPTIONS 요청에 Authorization이 없기 때문에 JwtFilter에서 문제 발생
      - spring cloud gateway를 사용하는 상점서버는 순서가 다를수도 있어서 되었던 것

--- src/backend/payment/src/main/java/com/steam/payment/global/filter/JwtFilter.java
+++ src/backend/payment/src/main/java/com/steam/payment/global/filter/JwtFilter.java
@@ -3,11 +3,14 @@ package com.steam.payment.global.filter;
 import com.steam.payment.global.common.UserContext;
 import com.steam.payment.global.common.UserDetails;
 import com.steam.payment.global.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Enumeration;
 
+@Slf4j
 public class JwtFilter implements Filter {
     private final String AUTHORIZATION_HEADER = "Authorization";
     private final String TOKEN_PREFIX = "Bearer ";
@@ -20,11 +23,25 @@ public class JwtFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String accessToken = request.getHeader(AUTHORIZATION_HEADER).substring(TOKEN_PREFIX.length());
-        UserDetails userDetails = JwtUtil.getPayload(accessToken);
-        UserContext.setUserDetails(userDetails);
+        log.info(request.getMethod());
+        if(request.getMethod().equals("OPTIONS"))
+            chain.doFilter(servletRequest, servletResponse);
+        else {
+            Enumeration<String> headerNames = request.getHeaderNames();
 
-        chain.doFilter(servletRequest, servletResponse);
+            if (headerNames != null) {
+                while (headerNames.hasMoreElements()) {
+                    String header = headerNames.nextElement();
+                    System.out.println(header + ": " + request.getHeader(header));
+                }
+            }
+
+            String accessToken = request.getHeader(AUTHORIZATION_HEADER).substring(TOKEN_PREFIX.length());
+            UserDetails userDetails = JwtUtil.getPayload(accessToken);
+            UserContext.setUserDetails(userDetails);
+
+            chain.doFilter(servletRequest, servletResponse);
+        }
     }
 
     @Override
