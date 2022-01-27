868325f3775eb48b0efcb6705ebbdc7c2bf83fe2
ummaeha <raya000@naver.com>
2022-01-27 11:50:00 +0900

    chore: axiosClient 적용하기위해서 임시로 올리는 커밋

--- src/frontend/pages/api/user/api.ts
+++ src/frontend/pages/api/user/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { axiosClient } from 'pages/api/axiosClient';
 import { IGetGiftCardListReqType, IResType } from './type';
 
 export async function getGiftCardListAPI(param: IGetGiftCardListReqType) {
-  const response = await axios.get<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_CHARGE}/payment/giftcards/KR`);
+  const response = await axiosClient.get<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_CHARGE}/payment/giftcards/KR`);
   // const response = await axios.get<IGetCategoriesResType>(`${process.env.NEXT_PUBLIC_BASE_URL}/store/categories`, {
   //   params: {},
   // });
