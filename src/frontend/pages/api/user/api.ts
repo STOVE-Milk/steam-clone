6d73129c03ddaf4be14a55ec9afa2d8eff6ec278
ummaeha <raya000@naver.com>
2022-02-06 16:38:07 +0900

    chore: 인증서버 base url설정

--- src/frontend/pages/api/user/api.ts
+++ src/frontend/pages/api/user/api.ts
@@ -3,16 +3,16 @@ import { axiosClient } from '../axiosClient';
 import { IDoSignupReqType, IResType, ICheckEmailReqType, IDoLogInReqType } from './type';
 
 export async function doSignupAPI(param: IDoSignupReqType) {
-  const response = await axiosClient.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/signup}`, param);
+  const response = await axiosClient.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_AUTH}/auth/signup}`, param);
   return response.data;
 }
 
 export async function checkEmailAPI(param: ICheckEmailReqType) {
-  const response = await axiosClient.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/email}`, param);
+  const response = await axiosClient.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_AUTH}/auth/email}`, param);
   return response.data;
 }
 
 export async function doLogInAPI(param: IDoLogInReqType) {
-  const response = await axiosClient.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/login}`, param);
+  const response = await axiosClient.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_AUTH}/auth/login}`, param);
   return response.data;
 }
