4a82d4a26050e10309e0d658010cb8cefbd235a7
minjyo <minjyo8823@gmail.com>
2022-02-07 11:22:47 +0900

    feat: mypage 뷰 추가

--- src/frontend/components/organisms/UserInfo.tsx
+++ src/frontend/components/organisms/UserInfo.tsx
@@ -19,24 +19,37 @@ const Wrapper = styled.div`
   border-radius: 10px;
   background: ${(props) => props.theme.colors.secondaryBg};
   padding: 1rem;
+  margin-top: 3rem;
 `;
 
-const UserSection = styled.div``;
+const UserSection = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const AddFriendBtn = styled(FilledButton)`
+  margin-left: auto;
+`;
 
 const ProfileSection = styled.div`
   display: flex;
+  flex-direction: row;
+  padding-top: 1rem;
 `;
 
 const ProfileImage = styled.div`
   border: 1px solid white;
   border-radius: 10px;
+  width: 10rem;
+  height: 10rem;
 `;
 
 const Desc = styled.div`
   border-radius: 10px;
-  border: 1px solid white;
+  background: ${(props) => props.theme.colors.secondaryText};
   flex: 1;
   margin-left: 1rem;
+  padding: 1rem;
 `;
 
 export default function UserInfo(props: IUserInfoProps) {
@@ -44,13 +57,11 @@ export default function UserInfo(props: IUserInfoProps) {
     <Wrapper>
       <UserSection>
         <Text types="large">{`${props.nickname} 님`}</Text>
-        {props.isFriend ? null : <FilledButton types="primary">친구 추가</FilledButton>}
+        {props.isFriend ? null : <AddFriendBtn types="primary">친구 추가</AddFriendBtn>}
       </UserSection>
       <ProfileSection>
-        <ProfileImage>
-          <Image src={props.profile.image}></Image>
-          <Desc>{props.profile.description}</Desc>
-        </ProfileImage>
+        <ProfileImage>{props.profile.image ? <Image src={props.profile.image}></Image> : null}</ProfileImage>
+        <Desc>{props.profile.description}</Desc>
       </ProfileSection>
     </Wrapper>
   );
--- src/frontend/pages/mypage.tsx
+++ src/frontend/pages/mypage.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next';
 import styled from 'styled-components';
 
 import Text from 'components/atoms/Text';
+import UserInfo from 'components/organisms/UserInfo';
 
 const Wrapper = styled.div`
   padding: 3rem;
@@ -12,6 +13,15 @@ const MyPage: NextPage = () => {
   return (
     <Wrapper>
       <Text types={'title'}>마이페이지</Text>
+      <UserInfo
+        id={1}
+        nickname={'user1'}
+        isFriend={false}
+        profile={{
+          description: 'desc',
+          image: '',
+        }}
+      ></UserInfo>
     </Wrapper>
   );
 };
