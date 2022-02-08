2e29ae340e6d66fbc46db4a74f74c9b51cd16261
ummaeha <raya000@naver.com>
2022-02-08 15:13:55 +0900

    chore: wishlist 페이지 가운데 정렬

--- src/frontend/pages/wishlist.tsx
+++ src/frontend/pages/wishlist.tsx
@@ -12,6 +12,11 @@ const TitleStyle = styled(Text)`
   margin-bottom: 2rem;
 `;
 
+const WishListWrapper = styled.div`
+  width: 80%;
+  margin: 2rem auto;
+`;
+
 const wishlist: NextPage<IState> = () => {
   const { wishList } = useSelector((state: IState) => state.game);
   const dispatch = useDispatch();
@@ -19,9 +24,8 @@ const wishlist: NextPage<IState> = () => {
   useEffect(() => {
     dispatch(getWishList.request({}));
   }, []);
-
   return (
-    <div>
+    <WishListWrapper>
       <TitleStyle types="large">WISH 게임 리스트</TitleStyle>
       {/*(성현) wishlist가 없으면 데이터가 아예 안와서 Undefined 처리 해놓음! 나중에 빈배열로오면 length 로 고치면 될 듯*/}
       {wishList.data === undefined
@@ -29,7 +33,7 @@ const wishlist: NextPage<IState> = () => {
         : wishList.data.map((eachGame) => {
             return <GameInfo key={eachGame.id} {...eachGame} />;
           })}
-    </div>
+    </WishListWrapper>
   );
 };
 
