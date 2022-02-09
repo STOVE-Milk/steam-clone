743fd26f51d5245fd48680a824a070720349f2cc
ummaeha <raya000@naver.com>
2022-02-09 19:34:10 +0900

    feat: next에서 ES Module import 하는 부분  에러해결 레퍼런스 1. https://github.com/konvajs/react-konva/issues/588 2. https://nextjs.org/docs/advanced-features/dynamic-import
    요약:
    1. /node_modules/react-konva/package.json 파일에 "type": "module" 추가
    2. 해당 컴포넌트를 불러오는 페이지에서 dynamic import 처리

--- src/frontend/pages/library.tsx
+++ src/frontend/pages/library.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
-
 import { IState } from 'modules';
 
+import dynamic from 'next/dynamic';
+
+const NoSSRMap = dynamic(() => import('components/organisms/Map'), {
+  ssr: false,
+});
+
 const library: NextPage<IState> = () => {
-  return <div></div>;
+  return <NoSSRMap />;
 };
 
 export default library;
