cba073de3e915161e18793d085fc8dcd9ee70530
ummaeha <raya000@naver.com>
2022-02-09 11:42:45 +0900

    feat: library 페이지 생성

---
+++ src/frontend/pages/library.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import type { NextPage } from 'next';
+
+import { IState } from 'modules';
+
+const library: NextPage<IState> = () => {
+  return <div></div>;
+};
+
+export default library;
