7eecb554693a6b6617fe2307ab9f8564e0e13ce7
ummaeha <raya000@naver.com>
2022-02-10 14:04:25 +0900

    chore: uri 맞춰서 env 와 api요청uri 수정

--- src/frontend/pages/api/game/api.ts
+++ src/frontend/pages/api/game/api.ts
@@ -14,7 +14,7 @@ import {
 } from './type';
 
 export async function getCategoriesAPI(param: IGetCategoriesReqType) {
-  const response = await axiosClient.get<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL}/store/categories`);
+  const response = await axiosClient.get<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_STORE}/store/categories`);
   // const response = await axios.get<IGetCategoriesResType>(`${process.env.NEXT_PUBLIC_BASE_URL}/store/categories`, {
   //   params: {},
   // });
@@ -24,19 +24,19 @@ export async function getCategoriesAPI(param: IGetCategoriesReqType) {
 
 export async function getGamesByCategoryAPI(param: IGetGamesByCategoryReqType) {
   const response = await axios.get<IResType>(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/store/games?category=${param.category}&page=0&size=5&sort=sale,desc`,
+    `${process.env.NEXT_PUBLIC_BASE_URL_STORE}/store/games?category=${param.category}&page=0&size=5&sort=sale,desc`,
   );
   return response.data;
 }
 
 export async function getGameAPI(param: IGetGameReqType) {
-  const response = await axios.get<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL}/store/detail/${param.id}`);
+  const response = await axios.get<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_STORE}/store/detail/${param.id}`);
 
   return response.data;
 }
 
 export async function purchaseGameAPI(param: IPurchaseGameReqType[]) {
-  const response = await axiosClient.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL}/payment/cart/purchase`, {
+  const response = await axiosClient.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_STORE}/payment/cart/purchase`, {
     games: param,
   });
 
@@ -50,13 +50,13 @@ export async function getGameInfoByIdListAPI(param: IGetGameInfoByIdListReqType)
 }
 // 찜 목록 불러오기
 export async function getWishListAPI(param: IGetWishListReqType) {
-  const response = await axiosClient.get<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL}/store/wishes`);
+  const response = await axiosClient.get<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_STORE}/store/wishes`);
 
   return response.data;
 }
 // 찜 하기
 export async function doWishAPI(param: IDoWishReqType) {
-  const response = await axiosClient.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL}/store/wishes`, param);
+  const response = await axiosClient.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_STOREL}/store/wishes`, param);
 
   return response.data;
 }
@@ -70,7 +70,7 @@ export async function doUnWishAPI(param: IDoUnWishReqType) {
 }
 ///store/userdata
 export async function getUserDataAPI(param: IGetUserDataReqType) {
-  const response = await axiosClient.get<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL}/store/userdata`);
+  const response = await axiosClient.get<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_STORE}/store/userdata`);
 
   return response.data;
 }
