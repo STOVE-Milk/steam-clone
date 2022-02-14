839491e81a0ddf33f2b20fbfade165f657725504
minjyo <minjyo8823@gmail.com>
2022-02-15 08:38:36 +0900

    feat: 토큰 재발급 함수 추가

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle } from 'styles/global-styles';
@@ -7,10 +8,37 @@ import { theme } from 'styles/theme';
 import wrapper from 'modules/configureStore';
 import Layout from 'templates/Layout';
 
+import { parseToken } from 'util/parseToken';
+import { reIssueTokenAPI } from 'api/auth/api';
+
 import 'styles/globals.css';
 import 'styles/font.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const verifyToken = async () => {
+    const accessToken = localStorage.getItem('accessToken');
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    //토큰 만료시간 1분전
+    const accessTokenTime = parseToken(accessToken!).exp * 1000 - 60000;
+    const refreshTokenTime = parseToken(refreshToken!).exp * 1000 - 60000;
+
+    const router = useRouter();
+
+    //refreshToken이 만료되었다면 로그인 페이지로 이동
+    if (refreshTokenTime < Date.now()) {
+      router.push('/signin');
+    }
+    //accessToken이 만료되었다면 accessToken 재발급하여 다시 저장
+    else if (accessTokenTime < Date.now()) {
+      if (accessToken && refreshToken) {
+        const newAccessToken = (await reIssueTokenAPI({ accessToken, refreshToken, isUpdated: false })).data
+          .accessToken;
+        localStorage.setItem('accessToken', newAccessToken);
+      }
+    }
+  };
+
   return (
     //themeprovider를 이용해서 미리 정의한 테마 타입들을 뷰 단에서 사용
     <ThemeProvider theme={theme}>
