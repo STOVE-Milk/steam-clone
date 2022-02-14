0b649af372f459a5fe90b761b2264d1d0983dab0
ummaeha <raya000@naver.com>
2022-02-14 11:41:08 +0900

    feat: axiosClient Token 넣기 처리

--- src/frontend/api/axiosClient.ts
+++ src/frontend/api/axiosClient.ts
@@ -1,11 +1,15 @@
 import axios, { AxiosInstance } from 'axios';
 
-const tempToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImV4cCI6MTIwMDAwMDAwMH0.eyJpZHgiOjEsIm5pY2tuYW1lIjoidGVzdCIsInJvbGUiOjEsImNvdW50cnkiOiJLUiJ9.6P_NJvZhGGc4XAdVlm74rgIVIzPtXZbChaZCqBbUba4';
+let token: string | null = '';
+if (typeof window !== 'undefined') {
+  token = localStorage.getItem('accessToken') ? localStorage.getItem('accessToken') : '';
+}
+// const tempToken =
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImV4cCI6MTIwMDAwMDAwMH0.eyJpZHgiOjEsIm5pY2tuYW1lIjoidGVzdCIsInJvbGUiOjEsImNvdW50cnkiOiJLUiJ9.6P_NJvZhGGc4XAdVlm74rgIVIzPtXZbChaZCqBbUba4';
 
 export const axiosClient: AxiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer ' + tempToken,
+    Authorization: `Bearer ${token}`,
   },
 });
