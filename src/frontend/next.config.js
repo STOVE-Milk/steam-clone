79f81734d8c22fdbebe5b78b0c6b4804a392b697
ummaeha <raya000@naver.com>
2022-02-15 17:46:49 +0900

    chore: 상점 서버 주소 변경 및 환경설정 파일 정리

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     NEXT_PUBLIC_BASE_URL_CHARGE: 'http://fortice.iptime.org:8080',
     NEXT_PUBLIC_BASE_URL_MEMBERSHIP: 'http://fortice.iptime.org:8080',
-    NEXT_PUBLIC_BASE_URL_STORE: 'http://54.180.117.120:8100',
+    NEXT_PUBLIC_BASE_URL_STORE: 'http://fortice.iptime.org:8080',
     NEXT_PUBLIC_BASE_URL_AUTH: 'http://localhost:8101',
     NEXT_PUBLIC_BASE_URL_WS: 'ws://fortice.iptime.org:8080/library',
   },
