529ceb6b7059a8ff3b3ac2f49be0f498b3f9b64d
ummaeha <raya000@naver.com>
2022-02-15 15:02:18 +0900

    feat: 토큰 재발급 로직 추가 및 axiosClient에 함수 호출부 추가

--- src/frontend/api/auth/api.ts
+++ src/frontend/api/auth/api.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { IResType } from 'api/game/type';
-import { IDoSignupReqType, ICheckEmailReqType, IDoSignInReqType, ICheckNicknameReqType } from './type';
+import {
+  IDoSignupReqType,
+  ICheckEmailReqType,
+  IDoSignInReqType,
+  ICheckNicknameReqType,
+  reIssueTokenReqType,
+} from './type';
 
 export async function doSignupAPI(param: IDoSignupReqType) {
   const response = await axios.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_AUTH}/auth/signup`, param);
@@ -18,5 +24,9 @@ export async function checkNicknameAPI(param: ICheckNicknameReqType) {
 
 export async function doSignInAPI(param: IDoSignInReqType) {
   const response = await axios.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_AUTH}/auth/signin`, param);
+  return response.data;
+}
+export async function reIssueTokenAPI(param: reIssueTokenReqType) {
+  const response = await axios.post<IResType>(`${process.env.NEXT_PUBLIC_BASE_URL_AUTH}/auth/token`, param);
   return response.data;
 }
