7491303294fe5503d4df7b4ec435689c06340c6c
abc7468 <abc7468@naver.com>
2022-02-18 01:10:52 +0900

    fix : getGameListByUserID 게임 리스트 직접 주도록 수정

--- src/backend/gRPC-gateway/proto/store.pb.gw.go
+++ src/backend/gRPC-gateway/proto/store.pb.gw.go
@@ -286,7 +286,7 @@ func local_request_Store_GetGameListInWishlist_0(ctx context.Context, marshaler 
 
 }
 
-func request_Store_GetGameIdListByUserId_0(ctx context.Context, marshaler runtime.Marshaler, client StoreClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func request_Store_GetGameListByUserId_0(ctx context.Context, marshaler runtime.Marshaler, client StoreClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq UserIdQueryParamRequest
 	var metadata runtime.ServerMetadata
 
@@ -308,12 +308,12 @@ func request_Store_GetGameIdListByUserId_0(ctx context.Context, marshaler runtim
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "user_id", err)
 	}
 
-	msg, err := client.GetGameIdListByUserId(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.GetGameListByUserId(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_Store_GetGameIdListByUserId_0(ctx context.Context, marshaler runtime.Marshaler, server StoreServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func local_request_Store_GetGameListByUserId_0(ctx context.Context, marshaler runtime.Marshaler, server StoreServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq UserIdQueryParamRequest
 	var metadata runtime.ServerMetadata
 
@@ -335,7 +335,7 @@ func local_request_Store_GetGameIdListByUserId_0(ctx context.Context, marshaler 
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "user_id", err)
 	}
 
-	msg, err := server.GetGameIdListByUserId(ctx, &protoReq)
+	msg, err := server.GetGameListByUserId(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -911,7 +911,7 @@ func RegisterStoreHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 
 	})
 
-	mux.Handle("GET", pattern_Store_GetGameIdListByUserId_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Store_GetGameListByUserId_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -922,7 +922,7 @@ func RegisterStoreHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_Store_GetGameIdListByUserId_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_Store_GetGameListByUserId_0(rctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -930,7 +930,7 @@ func RegisterStoreHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 			return
 		}
 
-		forward_Store_GetGameIdListByUserId_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Store_GetGameListByUserId_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1299,7 +1299,7 @@ func RegisterStoreHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
-	mux.Handle("GET", pattern_Store_GetGameIdListByUserId_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Store_GetGameListByUserId_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
@@ -1308,14 +1308,14 @@ func RegisterStoreHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_Store_GetGameIdListByUserId_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_Store_GetGameListByUserId_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_Store_GetGameIdListByUserId_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Store_GetGameListByUserId_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1497,7 +1497,7 @@ var (
 
 	pattern_Store_GetGameListInWishlist_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"store", "wishes"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Store_GetGameIdListByUserId_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"store", "users", "user_id", "games"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Store_GetGameListByUserId_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"store", "users", "user_id", "games"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Store_GetSearchingGameList_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"store", "search"}, "", runtime.AssumeColonVerbOpt(true)))
 
@@ -1531,7 +1531,7 @@ var (
 
 	forward_Store_GetGameListInWishlist_0 = runtime.ForwardResponseMessage
 
-	forward_Store_GetGameIdListByUserId_0 = runtime.ForwardResponseMessage
+	forward_Store_GetGameListByUserId_0 = runtime.ForwardResponseMessage
 
 	forward_Store_GetSearchingGameList_0 = runtime.ForwardResponseMessage
 
